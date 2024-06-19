551a8423a4f258f7d3516b04078f5b8f6662e066
Manqing Liu <manqingliu@g.harvard.edu>
2024-06-19 13:53:59 -0400

    added current version of fine tune code for psid before early stopping

--- experiments/tuning/fine_tune_psid.py
+++ experiments/tuning/fine_tune_psid.py
@@ -1,23 +1,49 @@
-import ray
 import os
-from ray import train, tune
 import json
-from config import config_psid
-import torch
 from argparse import ArgumentParser
+
+import torch
+import ray
+from ray import train, tune
+from ray.tune import Stopper
+
+from experiments.tuning.config import config_psid
 from src.utils import log_results
 from src.data.data_preprocess import data_preprocess
 from src.experiment import experiment
 from src.train.lalonde_psid.train_metrics import std_rmse, ipw_rmse, cfcv_rmse
 
+class EarlyStoppingAtMinRMSE(Stopper):
+    def __init__(self, patience=5):
+        self.best_rmse = float("inf")
+        self.no_improvement_count = 0
+        self.patience = patience
+
+    def __call__(self, trial_id, result):
+        current_rmse = result["cfcv_rmse"]
+        if current_rmse < self.best_rmse:
+            self.best_rmse = current_rmse
+            self.no_improvement_count = 0
+        else:
+            self.no_improvement_count += 1
+
+        return self.no_improvement_count >= self.patience
+
+    def stop_all(self):
+        return False
+
+
 def fine_tune(config, dag, train_dataloader, val_dataloader, val_data):
-    predictions = experiment(config, dag, train_dataloader, val_dataloader, val_data, random_seed=False)
+    model_trained, train_loss, val_loss, predictions = experiment(config, dag,
+                                                                  train_dataloader,
+                                                                  val_dataloader, val_data, random_seed=False)
     std_rmse_ = std_rmse(predictions['pred_y_A0'], predictions['pred_y_A1'])
     ipw_rmse_ = ipw_rmse(predictions['y'], predictions['t'], predictions['t_prob'])
     cfcv_rmse_ = cfcv_rmse(predictions['y'], predictions['t'], predictions['pred_y_A0'],
                         predictions['pred_y_A1'], predictions['t_prob'])
 
-    train.report({"std_rmse":  std_rmse_, "ipw_rmse": ipw_rmse_, "cfcv_rmse": cfcv_rmse_})
+    train.report({"train_loss": train_loss, "val_loss": val_loss, "std_rmse":  std_rmse_,
+                  "ipw_rmse": ipw_rmse_, "cfcv_rmse": cfcv_rmse_})
 
 
 
@@ -51,8 +77,9 @@ if __name__ == '__main__':
     analysis = tune.run(
         fine_tune_new,
         config=config_psid,
-        num_samples=1,  # Number of times to sample from the hyperparameter space
+        num_samples=30,  # Number of times to sample from the hyperparameter space
         resources_per_trial={"cpu": 1, "gpu": 1 if torch.cuda.is_available() else 0},
+        stop={"cfcv_rmse": 1000}
     )
 
     best_trial_std = analysis.get_best_trial("std_rmse", "min", "last")
